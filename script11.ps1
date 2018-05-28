f47b2f4f4ac8a7e95ad30140235d5a91357907bc
PizZaYolo35 <florian.wagnier@hotmail.fr>
2018-05-28 16:12:06 +0200

    firt commit

--- script11.ps1
+++ script11.ps1
@@ -1,5 +1,7 @@
 ﻿#demande un nombre à l'utilisateur, affiche ensuite "ce nombre et pair" ou "ce nombre est impair" en fonction
 
+#test modif
+
 $test = Read-Host "Donne moi un nombre"
 $oui = $test%2
 
